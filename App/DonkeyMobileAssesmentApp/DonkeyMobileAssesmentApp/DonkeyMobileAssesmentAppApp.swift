4b3c3484924b774330447cfa106a55c692baa525
PaullGit <94172003+PaullGit@users.noreply.github.com>
2025-10-14 14:16:28 +0200

    Initial Setup

---
+++ App/DonkeyMobileAssesmentApp/DonkeyMobileAssesmentApp/DonkeyMobileAssesmentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DonkeyMobileAssesmentAppApp.swift
+//  DonkeyMobileAssesmentApp
+//
+//  Created by Paul Tolnor on 14/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct DonkeyMobileAssesmentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App/DonkeyMobileAssesmentApp/DonkeyMobileAssesmentAppTests/DonkeyMobileAssesmentAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DonkeyMobileAssesmentAppTests.swift
+//  DonkeyMobileAssesmentAppTests
+//
+//  Created by Paul Tolnor on 14/10/2025.
+//
+
+import Testing
+@testable import DonkeyMobileAssesmentApp
+
+struct DonkeyMobileAssesmentAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
