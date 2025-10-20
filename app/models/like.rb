8292b12f3c059fd3d95d46e52638a76bbeea20e7
JoostdeJager <joostdejager2002@outlook.com>
2025-10-20 10:40:24 +0200

    fix rubocop linting error

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,9 +2,9 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :likeable, polymorphic: true
 
-  validates :user_id, uniqueness: { 
-    scope: %i[likeable_type likeable_id], 
-    message: "has already liked this item" 
+  validates :user_id, uniqueness: {
+    scope: %i[likeable_type likeable_id],
+    message: "has already liked this item"
   }
   validate :user_cannot_like_own_content
 
